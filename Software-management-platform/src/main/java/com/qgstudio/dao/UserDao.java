d30ed37dc1fc8ad69e3cbe6de4b24f9cee639766
ouroborosno2 <764904497@qq.com>
2022-07-25 09:43:26 +0800

    fix:改正了所查表的表名

--- Software-management-platform/src/main/java/com/qgstudio/dao/UserDao.java
+++ Software-management-platform/src/main/java/com/qgstudio/dao/UserDao.java
@@ -9,22 +9,22 @@ import org.apache.ibatis.annotations.Update;
 import java.util.List;
 
 public interface UserDao {
-    @Insert("INSERT INTO USER VALUES()")
+    @Insert("INSERT INTO T_USER VALUES()")
     public int save(User user);
-    @Update("UPDATE USER SET name=#{name},password=#{password},phone_number=#{phone_number},email=#{email},permission=#{permission}")
+    @Update("UPDATE T_USER SET name=#{name},password=#{password},phone_number=#{phone_number},email=#{email},permission=#{permission}")
     public int update(User user);
-    @Delete("DELETE FROM USER WHERE user_id = #{user_id}")
+    @Delete("DELETE FROM T_USER WHERE user_id = #{user_id}")
     public int delete(int user_id);
 
-    @Select("SELECT * FROM USER WHERE user_id = #{user_id}")
+    @Select("SELECT * FROM T_USER WHERE user_id = #{user_id}")
     public User getById(int user_id);
-    @Select("SELECT * FROM USER WHERE username = #{username}")
+    @Select("SELECT * FROM T_USER WHERE username = #{username}")
     public User getByUsername(String username);
-    @Select("SELECT * FROM USER WHERE phone_number = #{phone_number}")
+    @Select("SELECT * FROM T_USER WHERE phone_number = #{phone_number}")
     public User getByPhone_number(int phone_number);
-    @Select("SELECT * FROM USER WHERE email = #{email}")
+    @Select("SELECT * FROM T_USER WHERE email = #{email}")
     public User getByEmail(String email);
 
-    @Select("SELECT * FROM USER")
+    @Select("SELECT * FROM T_USER")
     public List<User> getAll();
 }
