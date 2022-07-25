cd373c5ecba203e3bd64a4967f72d2f9984d8488
stopyc <e13242597082@outlook.com>
2022-07-25 13:37:22 +0800

    feat:新增登录功能

--- Software-management-platform/src/main/java/com/qgstudio/controller/ResultEnum.java
+++ Software-management-platform/src/main/java/com/qgstudio/controller/ResultEnum.java
@@ -15,7 +15,17 @@ public enum ResultEnum {
     SUCCESS(200,"成功"),
     RESOURCE_NOT_FOUND(404,"资源未找到"),
     PARAMETER_NOT_VALID(400,"参数不合法"),
-    SERVER_INTERNAL_ERROR(500,"服务器内部错误")
+    SERVER_INTERNAL_ERROR(500,"服务器内部错误"),
+
+
+
+    //用户模块 60XYZ, Z为1代表成功,0代表失败,XY代表方法
+    //失败
+    USERNAME_ERROR(60010, "用户名错误"),
+    PASSWORD_ERROR(60020, "密码错误"),
+
+
+    //产品模块 70XXY,
 
     ;
     /**
---
+++ Software-management-platform/src/main/java/com/qgstudio/dao/UserDao.java
@@ -0,0 +1,30 @@
+package com.qgstudio.dao;
+
+import com.qgstudio.po.User;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+public interface UserDao {
+    @Insert("INSERT INTO T_USER VALUES(null,#{username},#{password},#{phone_number},#{email},#{permission})")
+    public int save(User user);
+    @Update("UPDATE T_USER SET username=#{username},password=#{password},phone_number=#{phone_number},email=#{email},permission=#{permission}")
+    public int update(User user);
+    @Delete("DELETE FROM T_USER WHERE user_id = #{user_id}")
+    public int delete(int user_id);
+
+    @Select("SELECT * FROM T_USER WHERE user_id = #{user_id}")
+    public User getById(int user_id);
+    @Select("SELECT * FROM T_USER WHERE username = #{username}")
+    public User getByUsername(String username);
+    @Select("SELECT * FROM T_USER WHERE phone_number = #{phone_number}")
+    public User getByPhone_number(String phone_number);
+    @Select("SELECT * FROM T_USER WHERE email = #{email}")
+    public User getByEmail(String email);
+
+    @Select("SELECT * FROM T_USER")
+    public List<User> getAll();
+}
