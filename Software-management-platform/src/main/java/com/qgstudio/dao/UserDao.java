3a6ae50c53962f8ae4ce7635616c64581ba3d65d
ouroborosno2 <764904497@qq.com>
2022-07-25 09:30:09 +0800

    feat:增加了User和UserDao

---
+++ Software-management-platform/src/main/java/com/qgstudio/dao/UserDao.java
@@ -0,0 +1,30 @@
+package com.qgstudio.dao;
+
+import com.qgstudio.po.User;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+public interface UserDao {
+    @Insert("INSERT INTO USER VALUES()")
+    public int save(User user);
+    @Update("UPDATE USER SET name=#{name},password=#{password},phone_number=#{phone_number},email=#{email},permission=#{permission}")
+    public int update(User user);
+    @Delete("DELETE FROM USER WHERE user_id = #{user_id}")
+    public int delete(int user_id);
+
+    @Select("SELECT * FROM USER WHERE user_id = #{user_id}")
+    public User getById(int user_id);
+    @Select("SELECT * FROM USER WHERE username = #{username}")
+    public User getByUsername(String username);
+    @Select("SELECT * FROM USER WHERE phone_number = #{phone_number}")
+    public User getByPhone_number(int phone_number);
+    @Select("SELECT * FROM USER WHERE email = #{email}")
+    public User getByEmail(String email);
+
+    @Select("SELECT * FROM USER")
+    public List<User> getAll();
+}
---
+++ Software-management-platform/src/main/java/com/qgstudio/po/User.java
@@ -0,0 +1,70 @@
+package com.qgstudio.po;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+    private int user_id;//用户id
+    private String username;//用户名
+    private String password;//密码
+    private int phone_number;//手机号码
+    private String email;//邮箱
+    private int permission;//权限，0为普通用户，1为管理员
+
+
+    public User(int user_id, String username, String password, int phone_number, String email, int permission) {
+        this.user_id = user_id;
+        this.username = username;
+        this.password = password;
+        this.phone_number = phone_number;
+        this.email = email;
+        this.permission = permission;
+    }
+
+    public User() {
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone_number(int phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public int getPermission() {
+        return permission;
+    }
+
+    public void setPermission(int permission) {
+        this.permission = permission;
+    }
+}
